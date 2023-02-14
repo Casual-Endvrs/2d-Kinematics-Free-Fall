d7379afa14657c4e5201b0484b60c623508ac43b
Casual.Endvrs <casual.endvrs@gmail.com>
2023-02-14 13:07:39 -0700

    Working Data Point Selection Page

--- 2D-Kinematics-Exp.py
+++ 2D-Kinematics-Exp.py
@@ -6,78 +6,244 @@ import tempfile
 
 from cls_vid_fbf import vid_fbf
 
+import numpy as np
+
+
 st.set_page_config(layout="wide")
 
-if 'vid_fbf' not in st.session_state:
-    st.session_state['vid_fbf'] = vid_fbf()
+if "vid_fbf" not in st.session_state:
+    st.session_state["vid_fbf"] = vid_fbf()
+    st.session_state["img_coords_prev"] = None
+    st.session_state["img_coords"] = None
 
-st.title('2D Kinematics Experiment')
+    st.session_state["curr_activity"] = "Set Ball Location"
 
-uploaded_video = st.file_uploader(
-    label = "Choose video:", 
-    type = ["mp4", "mov"], 
-    accept_multiple_files = False, 
-    on_change = None, 
-    # help = "Help stirng"
-    )
-
-if uploaded_video is not None :
-    if 'src_video' not in st.session_state:
-        st.session_state['src_video'] = uploaded_video
-
-    print(uploaded_video)
-    print(type(uploaded_video))
+#! make auto-advance on click an option
 
 
-frame_skip = 25 # display every 300 frames
+def load_video():
+    if st.session_state["vid_fbf"].frames is None:
+        with tempfile.NamedTemporaryFile() as tmp_f:
+            video_file = st.session_state["source video"]
+            tmp_f.write(video_file.read())
+            st.session_state["vid_fbf"].load_video(tmp_f.name)
 
-if uploaded_video is not None: # run only when user uploads video
-    with tempfile.NamedTemporaryFile() as tmp_f:
-        tmp_f.write(uploaded_video.read())
-        st.session_state['vid_fbf'].load_video(tmp_f.name)
-    
 
-    vid = uploaded_video.name
-    st.markdown(f"""
-    ### Files
-    - {vid}
-    """,
-    unsafe_allow_html=True) # display file name
+def prep_frame_number():
+    frm_num = st.session_state["sldr_frm_num"]  #! - 1
+    frame_buffer.prep_frame_num(frm_num)
 
-    cur_frame = 0
-    success = True
 
-    frame = st.session_state['vid_fbf'].get_frame(9, 720)
+def display_frame():
+    frame = frame_buffer.get_frame()
+
+    if frame is None:
+        return None
+
     pil_img = Image.fromarray(frame)
 
-    # for i in range(2) :
-    #     cols = st.columns(1)
-    #     with cols[0] :
-    value = sic(
+    sic(
         pil_img,
-        key = "pil",
-        height = 720,
-        width = 1280
+        key="img_coords",
     )
 
-    st.write(value)
 
-    print()
-    print()
-    print()
-    print()
-    print(dir(st))
-    print()
-    print()
-    print()
+def prev_frame():
+    frame_buffer.prep_prev_frame()
 
 
-    # container_width = st.get_container_width
-    # st.write(container_width)
-
-    screen_width = st.width
-    st.write(screen_width)
+def next_frame():
+    frame_buffer.prep_next_frame()
 
 
+def remove_ball_marker():
+    frame_buffer.rm_ball_frame_loc()
+    prep_frame_number()
 
 
+def build_and_show_composite():
+    # frame_buffer.create_composite_image()
+    frame_buffer.prep_frame_num(-1)
+
+
+def set_curr_activity(activity_num: str):
+    st.session_state["img_coords"] = None
+    st.session_state["curr_activity"] = activity_num
+
+
+def change_resolution():
+    res = st.session_state["display_res"]
+    frame_buffer.set_display_resolution(resolution=res)
+    prep_frame_number()
+
+
+def set_ref_len_meters():
+    len_m = 0.01 * st.session_state["ref_length"]
+    frame_buffer.set_ref_len_meters(len_m)
+    frame_buffer.calc_m_per_pxl()
+
+
+def tst_img_coords_slctd():
+    prev_coords = st.session_state["img_coords_prev"]
+    curr_coords = st.session_state["img_coords"]
+
+    if (curr_coords is None) or (prev_coords == curr_coords):
+        return
+
+    if st.session_state["curr_activity"] == "Set Ball Location":
+        frame_buffer.set_ball_frame_loc(curr_coords)
+        st.session_state["img_coords_prev"] = curr_coords
+        next_frame()
+
+    elif st.session_state["curr_activity"] == "Set Length Markers":
+        frame_buffer.set_length_markers(curr_coords)
+        st.session_state["img_coords_prev"] = curr_coords
+        frame_buffer.calc_m_per_pxl()
+        prep_frame_number()
+        st.experimental_rerun()
+
+    elif st.session_state["curr_activity"] == "Set Plum Line":
+        frame_buffer.set_plum_line_marker(curr_coords)
+        st.session_state["img_coords_prev"] = curr_coords
+        frame_buffer.calc_frame_theta()
+        prep_frame_number()
+        st.experimental_rerun()
+
+    st.session_state["img_coords"] = None
+
+
+def panel_mjr_actions():
+    st.write("Section action to perform:")
+
+    cols = st.columns(6)
+    # 1. ball location
+    with cols[0]:
+        lbl = "Set Ball Location"
+        st.button(
+            lbl,
+            key="btn_set_ball_loc",
+            on_click=set_curr_activity,
+            args=(lbl,),
+        )
+        st.button(
+            "Remove Frame Marker",
+            key="btn_rm_frm_ball_mrkr",
+            on_click=remove_ball_marker,
+        )
+        #! this needs to be a local function so the display can be updated
+
+    # 2. length markers
+    with cols[1]:
+        lbl = "Set Length Markers"
+        st.button(
+            lbl,
+            key="btn_ln_mrkrs",
+            on_click=set_curr_activity,
+            args=(lbl,),
+        )
+        st.number_input(
+            "Length between markers (centimeters)",
+            key="ref_length",
+            min_value=0.0,
+            on_change=set_ref_len_meters,
+        )
+        if frame_buffer.m_per_pxl in [0, 0.0, None]:
+            m_per_pxl = "undefined"
+            pxl_per_m = "undefined"
+        else:
+            m_per_pxl = f"{frame_buffer.m_per_pxl:.5f} m/pxl"
+            pxl_per_m = f"{1./frame_buffer.m_per_pxl:.2f} pxl/m"
+        st.write(f"meters/pixel --> {m_per_pxl}")
+        st.write(f"pixel/meters --> {pxl_per_m}")
+
+    # 3. plum line
+    with cols[2]:
+        lbl = "Set Plum Line"
+        st.button(lbl, key="btn_plum_line", on_click=set_curr_activity, args=(lbl,))
+        if frame_buffer.image_theta is None:
+            theta = "undefined"
+        else:
+            theta_val = frame_buffer.image_theta * 180 / np.pi
+            theta = f"{theta_val:.2f} degrees"
+        st.write(f"image rotation --> {theta} degrees")
+        #! option to zoom on 1st click
+
+    # 4. set background frame
+    with cols[3]:
+        st.button(
+            "Set Background Frame",
+            key="btn_set_bkgrnd_frm",
+            on_click=frame_buffer.set_frame_as_bkgrnd,
+        )
+        st.write(f"Frame to use as background: {frame_buffer.bkgrnd_frm_num}")
+
+    # 5. process results & obtain fit
+    with cols[4]:
+        st.button(
+            "Show Composite Image",
+            key="btn_composite_img",
+            on_click=build_and_show_composite,
+        )
+
+    # 6. set image size
+    with cols[5]:
+        st.selectbox(
+            "Image size",
+            options=("original", "720p", "1080p", "2k"),
+            index=0,
+            key="display_res",
+            on_change=change_resolution,
+        )
+
+    curr_action = st.session_state["curr_activity"]
+    st.write(f"Current action --> {curr_action}")
+
+
+def panel_frame_controls():
+    # Video frame selection controls
+    cols = st.columns([1, 7, 1])
+
+    with cols[0]:
+        st.button("Previous Frame:", on_click=prev_frame, key="btn_prev_frm")
+
+    with cols[1]:
+        dfn = frame_buffer.display_frame_num
+        sldr_val = 0 if dfn is None else dfn
+        # sldr_val = 1 if dfn is None else dfn + 1
+        lst_frm = frame_buffer.ttl_frms - 1
+        st.slider(
+            "Select Frame:",
+            min_value=0,
+            # min_value=1,
+            max_value=lst_frm,
+            value=sldr_val,
+            step=1,
+            key="sldr_frm_num",
+            on_change=prep_frame_number,
+        )
+
+    with cols[2]:
+        st.button("Next Frame:", on_click=next_frame, key="btn_nxt_frm")
+
+
+frame_buffer = st.session_state["vid_fbf"]
+
+st.title("2D Kinematics Experiment")
+
+if frame_buffer.file_name is None:
+    st.file_uploader(
+        label="Choose video:",
+        type=["mp4", "mov"],
+        accept_multiple_files=False,
+        key="source video",
+        on_change=load_video,
+        # label_visibility="hidden",
+    )
+else:
+    panel_mjr_actions()
+
+    st.markdown("""---""")
+
+    tst_img_coords_slctd()
+    display_frame()
+    panel_frame_controls()
--- cls_vid_fbf.py
+++ cls_vid_fbf.py
@@ -1,50 +1,502 @@
 import numpy as np
+from lmfit import Model, Parameters
 import cv2
+from PIL import Image, ImageDraw
 from pathlib import Path
+from typing import Union, List, Any, Dict
+
+#! Add return statement type hints
+#! Assumes each data point is sequential
+
 
 class vid_fbf:
     # Class to store a video frame-by-frame with per-frame information.
 
     def __init__(self):
-        self.file_name: str = None
-        self.frames: list[int] = None
-        self.ttl_frms: int = None
-        self.display_frame: int = 0
-    
-    def load_video(self, file_path: Path):
-        self.frames = []
-        self.ttl_frms = 0
-        self.display_frame = 0
+        self.file_name: Union[None, str] = None
+        self.frames_src: Union[None, List[int]] = None
+        self.frames: Union[None, List[int]] = None
+        self.ttl_frms: Union[None, int] = None
+        self.display_frame_num: Union[None, int] = None
+
+        self.frame_image: Union[None, np.ndarray] = None
+
+        #! Video editing variables
+        self.composite_image: Union[None, List[int]] = None
+        self.bkgrnd_frm_num: int = 0
+        self.ball_radius = 15
+        self.source_resolution: Union[None, List[int]] = None
+        self.display_resolution: Union[None, str, List[int]] = None
+
+        #! 2D Kinematics specific variables
+        self.ball_frm_locs: Union[None, np.ndarray[(Any, 2), int]] = None
+        self.length_markers_locs: Union[None, np.ndarray[(Any, 2), int]] = None
+        self.plum_line_markers: Union[None, np.ndarray[(Any, 2), int]] = None
+        self.image_theta: Union[None, float] = None
+
+        self.x_fit_result = None
+        self.y_fit_result = None
+
+        #! Equation variables
+        self.frame_rate: float = 30.0
+        self.frame_time: float = 1.0 / self.frame_rate
+
+        self.ref_len_pxls: Union[None, int] = None
+        self.ref_len_m: Union[None, float] = None
+        self.m_per_pxl: Union[None, float] = None
+
+        #! location of ball markers in pxls & m
+        self.pos_pxls: Union[None, np.ndarray[(Any, 2), int]] = None
+        self.pos_m: Union[None, np.ndarray[(Any, 2), float]] = None
+
+    def load_video(self, file_path: Path, resolution: str = "original"):
+        self.display_frame_num = 0
 
         vidcap = cv2.VideoCapture(file_path)
+        self.frame_rate = vidcap.get(cv2.CAP_PROP_FPS)
+        self.frame_time = 1.0 / self.frame_rate
+        src_frms = []
 
         while True:
             is_frame, frame_img = vidcap.read()
 
-            if not is_frame :
+            if not is_frame:
                 break
 
-            self.frames.append(frame_img)
-            self.ttl_frms += 1
+            src_frms.append(frame_img)
 
-    def get_frame(self, frame_num:int = 0, target_res_h:int = 720):
+        print("Source size:")
+        print(f"\t{np.shape(src_frms[0])}")
 
-        fsr = int(5)
-        if 0 <= frame_num < self.ttl_frms :
-            frame = np.copy(self.frames[frame_num])
+        self.file_name = file_path
+        self.frames_src = np.array(src_frms)
+        self.ttl_frms = np.shape(self.frames_src)[0]
+        self.ball_frm_locs = np.zeros((self.ttl_frms, 2), dtype=int)
 
-            return frame
+        src_res = np.shape(self.frames_src[0])
+        self.source_resolution = [src_res[1], src_res[0]]
+
+        self.set_display_resolution(resolution="original")
+        self.resize_video(resolution)
+
+        self.prep_frame_num(frame_num=0)
+
+    def set_display_resolution(
+        self,
+        resolution: str = "1080p",
+        height: Union[None, int] = None,
+        width: Union[None, int] = None,
+        # force_aspect_ratio: bool = False,
+    ):
+        src_sz = np.shape(self.frames_src[0])
+
+        if height is not None:
+            if height > src_sz[0]:
+                print(f"height > {src_sz[0]}")
+                self.frames = np.copy(self.frames_src)
+                return
+            width = src_sz[1] * height / src_sz[0]
+        elif width is not None:
+            if width > src_sz[1]:
+                print(f"width > {src_sz[1]}")
+                self.frames = np.copy(self.frames_src)
+                return
+            height = src_sz[0] * width / src_sz[1]
+        elif resolution is not None:
+            if resolution == "original":
+                src_res = np.shape(self.frames_src[0])
+                height, width = src_res[0], src_res[1]
+            elif resolution == "360p":
+                height, width = 360, 640
+            elif resolution == "720p":
+                height, width = 720, 1280
+            elif resolution == "1080p":
+                height, width = 1080, 1920
+            elif resolution in ["1440p", "2k"]:
+                height, width = 1440, 2560
+            elif resolution in ["2160p", "4k"]:
+                height, width = 2160, 3840
+            elif resolution in ["4320p", "8k"]:
+                height, width = 4320, 7680
+            else:
+                print("resolution was not found")
+                height, width = None, None
+
+            print(f"using resolution --> {resolution}")
+            print(f"{width} x {height}")
+
+        if None not in [width, height]:
+            print("setting new resolution")
+            self.display_resolution = [width, height]
+        else:
+            print("forcing original resolution")
+            if self.frames_src is None:
+                self.display_resolution = None
+            else:
+                src_res = np.shape(self.frames_src[0])
+                self.display_resolution = [src_res[1], src_res[0]]
+
+        print(f"display res --> {self.display_resolution}")
+
+        self.resize_video()
+
+    def resize_display_frame(
+        self,
+        resolution: Union[None, str] = None,
+        height: Union[None, int] = None,
+        width: Union[None, int] = None,
+        # force_aspect_ratio: bool = False,
+    ):
+        # if resolution is not None or height is not None or width is not None:
+        if np.any([entry is not None for entry in [resolution, height, width]]):
+            self.set_display_resolution(resolution, height, width)
+
+        if self.display_resolution is None:
+            return
+
+        [width, height] = self.display_resolution
+
+        self.frame_image = cv2.resize(self.frame_image, (width, height))
+
+    def resize_video(
+        self,
+        resolution: Union[None, str] = None,
+        height: Union[None, int] = None,
+        width: Union[None, int] = None,
+        # force_aspect_ratio: bool = False,
+    ):
+        if resolution == "original":
+            self.frames = np.copy(self.frames_src)
+            return
+
+        # if resolution is not None or height is not None or width is not None:
+        if np.any([entry is not None for entry in [resolution, height, width]]):
+            self.set_display_resolution(resolution, height, width)
+
+        if self.display_resolution is None:
+            print("invalid: self.display_resolution is None")
+            return
+
+        [width, height] = self.display_resolution
+
+        self.frames = []
+        for frame in self.frames_src:
+            frame = np.copy(frame)
+            self.frames.append(cv2.resize(frame, (width, height)))
+
+        #! I don't like this - should be np.array
+        self.frames = list(self.frames)
+
+    def prep_frame_num(self, frame_num: Union[None, int] = None):
+        if frame_num is None:
+            if self.display_frame_num is None:
+                frame_num = 0
+            else:
+                frame_num = self.display_frame_num
+
+        if frame_num == -1:
+            self.display_frame_num = frame_num
+            self.create_composite_image()
+            self.frame_image = self.composite_image
+            # self.resize_display_frame()
+
+        elif 0 <= frame_num < self.ttl_frms:
+            self.display_frame_num = frame_num
+            self.frame_image = np.copy(self.frames_src[self.display_frame_num])
+            self.plot_ball_marker()
+            # self.plot_length_markers()
+            # self.plot_plum_line_markers()
+            # self.resize_display_frame()
+        else:
+            #! if an invalid frame number is requested then do???
+            self.frame_image = None
+            return
+
+        self.plot_length_markers()
+        self.plot_plum_line_markers()
+        self.resize_display_frame()
+
+    def get_frame(self, frame_num: Union[None, int] = None):
+        self.prep_frame_num(frame_num)
+
+        return self.frame_image
+
+    def prep_prev_frame(self):
+        prev_frame_idx = self.display_frame_num - 1
+        self.prep_frame_num(prev_frame_idx)
+
+    def prep_next_frame(self):
+        next_frame_idx = self.display_frame_num + 1
+        self.prep_frame_num(next_frame_idx)
+
+    #! ball markers
+    def set_ball_frame_loc(self, ball_loc: Dict):
+        if self.file_name is None:
+            return
+
+        x_loc, y_loc = self.scale_pixel_loc(
+            ball_loc["x"], ball_loc["y"], to_original_res=True
+        )
+
+        self.ball_frm_locs[self.display_frame_num, 0] = x_loc
+        self.ball_frm_locs[self.display_frame_num, 1] = y_loc
+
+    def rm_ball_frame_loc(self):
+        if self.file_name is None:
+            return
+
+        self.ball_frm_locs[self.display_frame_num, 0] = 0
+        self.ball_frm_locs[self.display_frame_num, 1] = 0
+
+    def plot_ball_marker(self):
+        print("plot_ball_marker")
+
+        ball_loc = self.ball_frm_locs[self.display_frame_num]
+
+        if ball_loc[0] == 0 and ball_loc[1] == 0:
+            return
+
+        print(f"\toriginal: {ball_loc}")
+        x_y = ball_loc
+        self.plot_marker(x_y=x_y, color=(0, 255, 0))
+
+    #! length markers
+    def set_length_markers(self, marker_loc: Dict):
+        x_y = self.scale_pixel_loc(
+            marker_loc["x"], marker_loc["y"], to_original_res=True
+        )
+
+        if self.length_markers_locs is None:
+            self.length_markers_locs = [x_y, x_y]
+        else:
+            self.length_markers_locs[0] = self.length_markers_locs[1]
+            self.length_markers_locs[1] = x_y
+
+    # self.ref_len_pxls: Union[None, int] = None
+    # self.ref_len_m: Union[None, float] = None
+    # self.m_per_pxl: Union[None, float] = None
+    # self.plum_line_markers: Union[None, np.ndarray[(Any, 2), int]] = None
+    # self.image_theta: Union[None, float] = None
+
+    def set_ref_len_meters(self, len_m: float):
+        self.ref_len_m = len_m
+
+    def calc_m_per_pxl(self):
+        if (self.ref_len_m is None) or (self.ref_len_m < 0):
+            self.m_per_pxl = None
+            print("invalid: self.ref_len_m is None or 0")
+            return
+
+        if self.length_markers_locs is None:
+            self.m_per_pxl = None
+            print("invalid: self.length_markers_locs is None")
+            return
+
+        dx_pxl = self.length_markers_locs[0][0] - self.length_markers_locs[1][0]
+        dy_pxl = self.length_markers_locs[0][1] - self.length_markers_locs[1][1]
+        dist_pxl = np.sqrt(dx_pxl**2 + dy_pxl**2)
+
+        if dist_pxl == 0:
+            self.m_per_pxl = None
+            print("invalid: dist_pxl == 0")
+            return
+
+        self.m_per_pxl = self.ref_len_m / dist_pxl
+
+    def plot_length_markers(self):
+        if self.length_markers_locs is None:
+            return
+
+        for x_y in self.length_markers_locs:
+            self.plot_marker(x_y=x_y, color=(0, 0, 255))
+
+    #! plum line markers
+    def set_plum_line_marker(self, marker_loc: Dict):
+        x_y = self.scale_pixel_loc(
+            marker_loc["x"], marker_loc["y"], to_original_res=True
+        )
+
+        if self.plum_line_markers is None:
+            self.plum_line_markers = [x_y, x_y]
+        else:
+            self.plum_line_markers[0] = self.plum_line_markers[1]
+            self.plum_line_markers[1] = x_y
+
+    def calc_frame_theta(self):
+        if self.plum_line_markers is None:
+            self.image_theta = None
+            print("invalid: self.plum_line_markers is None")
+            return
+
+        start_pnt = self.plum_line_markers[0]
+        end_pnt = self.plum_line_markers[1]
+        if start_pnt[1] > end_pnt[1]:
+            start_pnt, end_pnt = end_pnt, start_pnt
+
+        dx_pxl = end_pnt[0] - start_pnt[0]
+        dy_pxl = end_pnt[1] - start_pnt[1]
+
+        if dy_pxl == 0:
+            print("invalid: dy_pxl == 0")
+            self.image_theta = None
+            return
+        else:
+            self.image_theta = np.arctan(dx_pxl / dy_pxl)
+
+    def plot_plum_line_markers(self):
+        if self.plum_line_markers is None:
+            return
+
+        for x_y in self.plum_line_markers:
+            self.plot_marker(x_y=x_y, color=(156, 81, 182))
+
+    def plot_marker(
+        self,
+        x_y,
+        color=(255, 255, 255),
+        markerType=cv2.MARKER_CROSS,
+        markerSize=20,
+        thickness=2,
+        xy_is_src_res: bool = True,
+    ):
+        if not xy_is_src_res:
+            x_y = self.scale_pixel_loc(*x_y, to_original_res=True)
+
+        cv2.drawMarker(
+            self.frame_image, tuple(x_y), color, markerType, markerSize, thickness
+        )
+
+    #! Video editing methods
+    def scale_pixel_loc(
+        self, width: int, height: int, to_original_res: bool = True
+    ) -> List[int]:
+        if to_original_res:
+            width = width * self.source_resolution[0] / self.display_resolution[0]
+            height = height * self.source_resolution[1] / self.display_resolution[1]
+        else:
+            width = width * self.display_resolution[0] / self.source_resolution[0]
+            height = height * self.display_resolution[1] / self.source_resolution[1]
+
+        width = int(width)
+        height = int(height)
+
+        return [width, height]
+
+    def set_frame_as_bkgrnd(self):
+        if 0 <= self.display_frame_num < self.ttl_frms:
+            self.bkgrnd_frm_num = self.display_frame_num
+
+    def create_composite_image(self):
+        background = Image.fromarray(self.frames_src[self.bkgrnd_frm_num])
+
+        bkgrnd_shp = tuple(np.shape(background)[:2][::-1])
+
+        for idx in np.arange(self.ttl_frms):
+            ball_loc = self.ball_frm_locs[idx]
+            if ball_loc[0] == 0 and ball_loc[1] == 0:
+                continue
+
+            ball_frame = Image.fromarray(self.frames_src[idx])
+
+            xc, yc = ball_loc
+            x1 = xc - self.ball_radius
+            x2 = xc + self.ball_radius
+            y1 = yc - self.ball_radius
+            y2 = yc + self.ball_radius
+
+            mask = Image.new("L", bkgrnd_shp, 0)
+            draw = ImageDraw.Draw(mask)
+            draw.ellipse((x1, y1, x2, y2), fill=255)
+
+            background = Image.composite(ball_frame, background, mask)
+
+        self.composite_image = np.array(background)
+
+    def plot_kin_eqns(self):
+        if self.composite_image is None:
+            self.create_composite_image()
+
+        if None in [self.x_fit_result, self.y_fit_result]:
+            self.fit_data()
+
+        self.draw_parametric_fit()
+
+    def draw_parametric_fit(self):
+        draw_points = (
+            np.asarray([self.x_fit_result.best_fit, self.y_fit_result.best_fit]).T
+        ).astype(np.int32)
+        cv2.polylines(self.composite_image, [draw_points], False, (0, 255, 0))
+
+        # num_data_pnts = np.shape(self.pos_m)[0]
+
+        # steps_per_frm = 1
+        # num_steps = steps_per_frm * num_data_pnts
+        # t_max = self.frame_time * num_data_pnts
+        # ts = np.linspace(0, t_max, num_steps)
+        # dt = self.frame_time / steps_per_frm
+        # ts = np.arange(0, t_max+dt, dt)
+
+        # xs = self.k_displacement(ts, **self.x_fit_result.best_values)
+        # ys = self.k_displacement(ts, **self.y_fit_result.best_values)
+
+        # draw_points = (np.asarray([xs, ys]).T).astype(np.int32)
+        # cv2.polylines(self.frames[0], [draw_points], False, (255, 0, 0))
+
+    #! Equation variables
+    def calc_ball_displacements(self):
+        ball_has_pos = self.ball_frm_locs[:, 0] != 0
+        self.pos_pxls = self.ball_frm_locs[ball_has_pos, :]
+
+        #! origin translation should be moved to an export function
+        # # set origin to be at the first ball
+        # #   x-axis shift
+        # self.pos_pxls[:, 0] -= self.pos_pxls[0, 0]
+
+        # #   y-axis shift & flip --> flip as y-axis is inverted
+        # self.pos_pxls[:, 1] = -1 * self.pos_pxls[:, 1] + self.pos_pxls[0, 1]
+
+        # convert position in pxls --> meters
+        self.pos_m = self.pos_pxls * self.m_per_pxl
+
+    def fit_data(self):
+        self.fit_x_data()
+        self.fit_y_data()
+
+    def fit_x_data(self):
+        self.calc_ball_displacements()
+
+        data = self.pos_pxls[:, 0]
+        ts = self.frame_time * np.arange(len(data))
+
+        model = Model(self.k_displacement)
+        params = Parameters()
+        params.add("x_0", value=data[0], vary=False)
+        params.add("v_0", value=1)
+        params.add("a", value=0)
+        self.x_fit_result = model.fit(data, t=ts, params=params)
+
+    def fit_y_data(self):
+        self.calc_ball_displacements()
+
+        data = self.pos_pxls[:, 1]
+        ts = self.frame_time * np.arange(len(data))
+
+        model = Model(self.k_displacement)
+        params = Parameters()
+        params.add("x_0", value=data[0], vary=False)
+        params.add("v_0", value=1)
+        params.add("a", value=-10 / self.m_per_pxl)
+        self.y_fit_result = model.fit(data, t=ts, params=params)
+
+    @staticmethod
+    def k_displacement(t, x_0, v_0, a):
+        return x_0 + v_0 * t + 0.5 * a * t**2
 
 
-if __name__ == '__main__' :
-
-    file_path = '/home/braden/Git_Repos/test_video_processing/20230131_220018.mp4'
+if __name__ == "__main__":
+    file_path = "/home/braden/Git_Repos/test_video_processing/20230131_220018.mp4"
 
     x = vid_fbf()
     x.load_video(file_path)
 
     print(x.ttl_frms)
-
-
-
-
